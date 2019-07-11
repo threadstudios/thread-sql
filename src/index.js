3e5a6b5c0f5368ee5ab6e70d38551c185674faed
Paul Westerdale <paul@westerdale.me>
2019-07-11 13:33:14 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const mysql = require("mysql")
+
+const connection = mysql.createConnection(process.env.JAWSDB_URL ? process.env.JAWSDB_URL : {
+  host : process.env.MYSQL_HOST,
+  port : process.env.MYSQL_PORT,
+  user : process.env.MYSQL_USER,
+  password : process.env.MYSQL_PASSWORD,
+  database : process.env.MYSQL_DB
+})
+
+const sql = puresql.adapters.mysql(connection)
+
+export const sql;
